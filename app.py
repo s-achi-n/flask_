22a874f40c77bff5c48bafc5bac6ed9d991c0192
Sachin <kumar990sachin@gmail.com>
2021-07-30 18:12:55 +0530

    init

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy, sqlalchemy
 from datetime import datetime, timezone
@@ -61,10 +62,8 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
- 
-    
-    
 
 if __name__ =="__main__":
-    app.run(debug = True)
-
+    #app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
